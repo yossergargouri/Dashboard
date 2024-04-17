7e57251d1195af18c954df2d862b9cb415aab36d
yoser gargouri <gargouryyoser@gmail.com>
2024-04-17 09:22:59 +0100

    userslist

--- src/components/Charts/ChartThree.tsx
+++ src/components/Charts/ChartThree.tsx
@@ -63,7 +63,7 @@ const ChartThree: React.FC = () => {
   handleReset;
 
   return (
-    <div className="sm:px-7.5 col-span-12 rounded-sm border border-stroke bg-white px-5 pb-5 pt-7.5 shadow-default dark:border-strokedark dark:bg-boxdark xl:col-span-5">
+    <div className="sm:px-7.5 col-span-12 rounded-sm border border-stroke bg-white px-5 pb-5 pt-7.5 shadow-default dark:border-strokedark dark:bg-boxdark xl:col-span-6">
       <div className="mb-3 justify-between gap-4 sm:flex">
         <div>
           <h5 className="text-xl font-semibold text-black dark:text-white">
@@ -72,18 +72,15 @@ const ChartThree: React.FC = () => {
         </div>
         <div>
           <div className="relative z-20 inline-block">
-            <select
-              name=""
-              id=""
+            <div
+             
               className="relative z-20 inline-flex appearance-none bg-transparent py-1 pl-3 pr-8 text-sm font-medium outline-none"
             >
-              <option value="" className="dark:bg-boxdark">
+              <div className="dark:bg-boxdark">
                 Monthly
-              </option>
-              <option value="" className="dark:bg-boxdark">
-                Yearly
-              </option>
-            </select>
+              </div>
+             
+            </div>
             <span className="absolute right-3 top-1/2 z-10 -translate-y-1/2">
               <svg
                 width="10"
---
+++ src/components/Chat/Usersprofile.tsx
@@ -0,0 +1,63 @@
+import { Link } from 'react-router-dom';
+
+import UserOne from '../../images/user/user-01.png';
+import { Userprofile } from '../../types/Usersprofile';
+
+
+const UsersData: Userprofile[] = [
+  {
+    avatar: UserOne,
+    name: 'User name',
+    email: 'How are you?',
+    phonenumber: 12,
+    
+  },
+  {
+    avatar: UserOne,
+    name: 'User name',
+    email: 'How are you?',
+    phonenumber: 12,
+  },
+  {
+    avatar: UserOne,
+    name: 'User name',
+    email: 'How are you?',
+    phonenumber: 12,
+  },
+  
+];
+
+const Usersprofile = () => {
+  return (
+    <div className="col-span-40 rounded-sm border border-stroke bg-white py-6 shadow-default dark:border-strokedark dark:bg-boxdark xl:col-span-6">
+      <h4 className="mb-6 px-7.5 text-xl font-semibold text-black dark:text-white">
+        Users
+      </h4>
+
+      <div>
+        {UsersData.map((users, key) => (
+          <Link
+            to="/"
+            className="flex items-center gap-5 py-3 px-7.5 hover:bg-gray-3 dark:hover:bg-meta-4"
+            key={key}
+          >
+            <div className="relative h-14 w-14 rounded-full">
+              <img src={users.avatar} alt="User" />
+             
+            </div>
+
+            <div className="flex flex-1 items-center justify-between">
+              <div>
+                <h5 className="font-medium text-black dark:text-white">
+                  {users.name}
+                </h5>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Usersprofile;
