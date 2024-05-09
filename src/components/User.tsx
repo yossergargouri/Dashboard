a4504bb759b29efcd80019720980daabd6dc6330
yoser gargouri <gargouryyoser@gmail.com>
2024-05-09 14:09:30 +0100

    usersList

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -8,7 +8,7 @@ const Usersprofile = () => {
   useEffect(() => {
     const fetchUserData = async () => {
       try {
-        const response = await axios.get('http://localhost:5245/User'); // Remplacez '/api/users' par l'URL de votre point de terminaison backend
+        const response = await axios.get('http://localhost:5245/User'); 
         setUsersData(response.data);
       } catch (error) {
         console.error('Error fetching user data:', error);
